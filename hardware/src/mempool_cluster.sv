d51b598f34dfea3b128ab4cd5b018bb943239be3
viv-eth <vivianep@ethz.ch>
2022-11-28 13:29:48 +0100

    hardware: modify outstanding transactions of DMA

--- hardware/src/mempool_cluster.sv
+++ hardware/src/mempool_cluster.sv
@@ -109,7 +109,7 @@ module mempool_cluster
     .DmaRegionWidth (NumBanksPerGroup*4   ),
     .DmaRegionStart (TCDMBaseAddr         ),
     .DmaRegionEnd   (TCDMBaseAddr+TCDMSize),
-    .TransFifoDepth (16                   ),
+    .TransFifoDepth (64                   ),
     .burst_req_t    (dma_req_t            ),
     .meta_t         (dma_meta_t           )
   ) i_idma_distributed_midend (
