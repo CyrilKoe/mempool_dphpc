cadfaa97bccb861cb3c53672bbd7010984355f60
viv-eth <vivianep@ethz.ch>
2022-12-14 18:11:15 +0100

    scripts: add custom benchmark script

--- hardware/scripts/gen_avg.py
+++ hardware/scripts/gen_avg.py
@@ -32,12 +32,12 @@ for files in os.listdir(path):
 
         print("\n")
         print("*******************************")
-        print("**    AVERAGE PERFORMANCE    **")
+        print("**    Overall PERFORMANCE    **")
         print("*******************************")
 
         print("")
         for section in set(csvread['section']):
-            print("Section %d:\n" % section)
+            print("Section %d:                      Average                 Maximum                  Minimum \n" % section)
             sectionread = csvread.loc[csvread['section'] == section]
             keys = csvread.columns
             remove_keys = ['core',
@@ -55,37 +55,9 @@ for files in os.listdir(path):
                     column = sectionread[key].replace(np.nan, 0)
                     column = column.to_numpy()
                     avg = np.average(column)
+                    worst = np.max(column)
+                    best = np.min(column)
                 except Exception:
                     # Key could not be averaged
                     continue
-                print("%-30s %4.4f" % (key, avg))
-
-        print("\n")
-        print("*******************************")
-        print("**  WORST CASE PERFORMANCE   **")
-        print("*******************************")
-
-        print("")
-        for section in set(csvread['section']):
-            print("Section %d:\n" % section)
-            sectionread = csvread.loc[csvread['section'] == section]
-            keys = csvread.columns
-            remove_keys = ['core',
-                           'section',
-                           'start',
-                           'end',
-                           'snitch_load_latency',
-                           'snitch_load_region',
-                           'snitch_load_tile',
-                           'snitch_store_region',
-                           'snitch_store_tile']
-            keys = keys.drop(remove_keys, errors='raise')
-            for key in keys:
-                try:
-                    column = sectionread[key].replace(np.nan, 0)
-                    column = column.to_numpy()
-                    max = np.max(column)
-                except Exception:
-                    # Key could not be averaged
-                    continue
-                print("%-30s %4.4f" % (key, max))
+                print("%-30s %4.4f              %4.4f              %4.4f" % (key, avg, worst, best))
