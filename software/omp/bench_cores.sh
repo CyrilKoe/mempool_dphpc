9f7586d510ae7afbb1d0326c1289b123237ae014
Cyril Koenig <cykoenig@iis.ee.ethz.ch>
2022-12-18 21:21:01 +0100

    Final presentation

--- software/omp/bench_cores.sh
+++ software/omp/bench_cores.sh
@@ -1,27 +1,26 @@
-datas_to_bench="2 4 8 16 32 64 128 200"
+datas_to_bench="2 64 200"
 cores_to_bench="2 4 8 16 32 64 128 256"
 progs_to_bench="my_argmax_dyn_3"
 
-for iteration in {1..10}
+for iteration in {1..20}
 do
     for PROG_TO_BENCH in $progs_to_bench
     do
         for DATA_GEN_SIZE in $datas_to_bench
         do
             SEED=$(date +%s)
-            MAX=$(echo ${SEED}%10000 | bc)
+            MAX=$(echo ${SEED}%2500 | bc)
             (cd my_argmax_data && make DATA_GEN_SIZE=${DATA_GEN_SIZE} DATA_GEN_SEED=${SEED} MAX=${MAX} data.h -B)
 
             # Bench the base case
-            RESULT_PATH="results/my_argmax_dyn_base_1_${DATA_GEN_SIZE}"
+            RESULT_PATH="results/my_argmax_dyn_base_1_${DATA_GEN_SIZE}_${MAX}_${SEED}"
 
-            #make my_argmax_dyn_base
-            #(cd ../../hardware/ && app=omp/my_argmax_dyn_base make simcvcs && make resultpath=${RESULT_PATH} trace)
+            make my_argmax_dyn_base
+            (cd ../../hardware/ && app=omp/my_argmax_dyn_base make simcvcs && make resultpath=${RESULT_PATH} trace)
 
             for NUM_CORES_BENCH in $cores_to_bench
             do
                 RESULT_PATH="results/${PROG_TO_BENCH}_${NUM_CORES_BENCH}_${DATA_GEN_SIZE}_${MAX}_${SEED}"
-                #(cd ${PROG_TO_BENCH} && make NUM_CORES_BENCH=${NUM_CORES_BENCH} run -B)
                 make NUM_CORES_BENCH=${NUM_CORES_BENCH} ${PROG_TO_BENCH}
                 (cd ../../hardware/ && app=omp/${PROG_TO_BENCH} make simcvcs && make resultpath=${RESULT_PATH} trace)
             done
