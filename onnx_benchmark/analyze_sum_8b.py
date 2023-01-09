efbdd1843acc301cc62dcc33389fae7cf6ccc947
Gamze Islamoglu <gislamoglu@iis.ee.ethz.ch>
2023-01-09 22:24:07 +0100

    Update benchmark folder for onnx reductions

--- onnx_benchmark/analyze_sum_8b.py
+++ onnx_benchmark/analyze_sum_8b.py
@@ -13,13 +13,13 @@ import pandas as pd
 import numpy as np
 
 ext = ('.csv')
-path = 'ReduceSum8_new'
+path = 'reduceSum_8b'
 os.chdir(path)
 main_path = os.getcwd()
 # print(main_path)
 
 df = pd.DataFrame()
-cycles = np.zeros(12)
+cycles = np.zeros(13)
 print(cycles)
 
 for folders in os.listdir(main_path):
@@ -40,8 +40,8 @@ for folders in os.listdir(main_path):
                 max = np.max(column)
                 # print("%-30s %4.4f" % (key, max))
                 cycles[section] = max
-            df_tmp = pd.DataFrame(cycles, index=['seq', 'man1', 'man2', 'man3', 'man4', 'seq_simd', 'man1_simd', 'man2_simd', 'man3_simd', 'man4_simd', 'omp_atomic', 'omp_stat'], columns=[folders])
+            df_tmp = pd.DataFrame(cycles, index=['seq', 'man1', 'man2', 'man3', 'man4', 'man_amo', 'seq_simd', 'man1_simd', 'man2_simd', 'man3_simd', 'man4_simd', 'simd_atomic', 'omp_stat'], columns=[folders])
             df = pd.concat([df, df_tmp], axis=1)
     os.chdir(main_path)
 print(df)
-df.to_csv('../ReduceSum8_amo.csv')
+df.to_csv('../reduceSum_8b.csv')
--- onnx_benchmark/script_inputsize.sh
+++ onnx_benchmark/script_inputsize.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-export mempool_path=/scratch2/gislamoglu/hpc/mempool
-export app=onnx_ReduceSum8
+export mempool_path=../
+export app=reduceSum
 export script_path=$mempool_path/onnx_benchmark
-export folder_path=$mempool_path/onnx_benchmark/ReduceSum8_new
+export folder_path=$mempool_path/onnx_benchmark/reduceSum
 
 mkdir $folder_path
 
@@ -11,12 +11,12 @@ for i in 4 8 16 32 64 128 256 512
 do
   export input_size=$i
 
-  cd $mempool_path/software/omp
+  cd $mempool_path/software/onnx
   make $app IS=$input_size  
   save_path=$folder_path/$input_size
 
   cd $mempool_path/hardware
-  make simcvcs app=omp/$app
+  make simcvcs app=onnx/$app
   export result_dir=$save_path
   make trace
 done
